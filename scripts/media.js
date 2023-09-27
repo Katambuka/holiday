4bb91eaa7a1dd8a667f35d790e69ece830425ba6
Katambuka <kan22001@byui.edu>
2023-09-27 14:18:28 +0400

    nvd

--- scripts/media.js
+++ scripts/media.js
@@ -42,4 +42,45 @@ volumeSlider.addEventListener("input", () => {
 
 audioSource.connect(gainNode).connect(audioCtx.destination);
 
+const em = document.createElement("em"); // create a new em element
+const para = document.querySelector("p"); // reference an existing p element
+em.textContent = "Hello there!"; // give em some text content
+para.appendChild(em); // embed em inside para
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+// play/pause audio
+playBtn.addEventListener("click", () => {
+  // check if context is in suspended state (autoplay policy)
+  if (audioCtx.state === "suspended") {
+    audioCtx.resume();
+  }
+
+  // if track is stopped, play it
+  if (playBtn.getAttribute("class") === "paused") {
+    audioElement.play();
+    playBtn.setAttribute("class", "playing");
+    playBtn.textContent = "Pause";
+    // if track is playing, stop it
+  } else if (playBtn.getAttribute("class") === "playing") {
+    audioElement.pause();
+    playBtn.setAttribute("class", "paused");
+    playBtn.textContent = "Play";
+  }
+});
+
+// if track ends
+audioElement.addEventListener("ended", () => {
+  playBtn.setAttribute("class", "paused");
+  playBtn.textContent = "Play";
+});
+
  
